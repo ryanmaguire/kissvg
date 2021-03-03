a2f2a4f9bde7ff9d1633a3ae1f3d066145617ceb
Ryan Maguire <ryan_maguire@student.uml.edu>
2021-03-03 13:11:58 -0500

    Copying code from Mathematics-and-Physics project to here.

---
+++ src/config.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+CC=gcc
+
+echo "Clearing older files..."
+rm -f *.so
+rm -f *.o
+
+echo "Compiling KissVG..."
+CompilerArgs1="-std=c89 -ansi -pedantic -pedantic-errors -Wall -Wextra"
+CompilerArgs2="-Wpedantic -Wmisleading-indentation"
+CompilerArgs3="-Wmissing-prototypes -Wold-style-definition"
+CompilerArgs4="-Wstrict-prototypes -I../../ -DNDEBUG -g -fPIC -O2 -c"
+CompilerArgs="$CompilerArgs1 $CompilerArgs2 $CompilerArgs3 $CompilerArgs4"
+
+echo -e "\n\tCompiler Options:"
+echo -e "\t\t$CompilerArgs1"
+echo -e "\t\t$CompilerArgs2"
+echo -e "\t\t$CompilerArgs3"
+echo -e "\t\t$CompilerArgs4"
+
+
+for filename in ./*.c; do
+    echo -e "\t\tCompiling: $filename"
+    $CC $CompilerArgs $filename
+done
+
+#   sharedobjectlist=""
+#   for filename in ./*.o; do
+#       sharedobjectlist="$sharedobjectlist $filename"
+#   done
+
+# echo "Moving to /usr/local/lib/libkissvg.so"
+# sudo mv libkissvg.so /usr/local/lib/libkissvg.so
+
+echo "Cleaning up..."
+rm -f *.o
+
+echo "Done"
